61e1dc607cb0e648dbb0e241efa5a6bb43be7be8
YuverBecerra <161131054+YuverBecerra@users.noreply.github.com>
2024-05-05 12:35:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from frontend.Estructuras.area_subtitulos.subtitulos import variableB
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server 
+
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(variableA, md=12, style={'textAlign': 'center'}),
